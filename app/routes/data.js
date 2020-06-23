05847854307108818dc48bf3c8d3a6225c0e87ab
John Watson <johnwatson484@hotmail.com>
2020-06-23 20:57:15 +0100

    Logout added

--- app/routes/data.js
+++ app/routes/data.js
@@ -5,7 +5,7 @@ module.exports = [{
   method: 'GET',
   path: '/data',
   handler: async (request, h) => {
-    const token = request.state.token
+    const token = request.state.dl_token
     const { payload } = await wreck.get(`${config.apiGatewayHost}/restricted-role`, {
       payload: request.payload,
       headers: {
