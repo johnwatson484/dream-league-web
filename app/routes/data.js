fd6c46af33f1e640648b8029f077ecafb9efaac2
John Watson <johnwatson484@hotmail.com>
2020-06-27 19:36:08 +0100

    Update routes

--- app/routes/data.js
+++ app/routes/data.js
@@ -1,9 +1,13 @@
 const wreck = require('wreck')
 const config = require('../config')
+const mapAuth = require('../handlers/map-auth')
 
 module.exports = [{
   method: 'GET',
   path: '/data',
+  config: {
+    pre: [{ method: mapAuth, assign: 'auth' }]
+  },
   handler: async (request, h) => {
     const token = request.state.dl_token
     const { payload } = await wreck.get(`${config.apiGatewayHost}/restricted-role`, {
@@ -12,6 +16,6 @@ module.exports = [{
         Authorization: token || ''
       }
     })
-    return h.view('success', { model: payload })
+    return h.view('success', { model: payload, auth: request.pre.auth })
   }
 }]
