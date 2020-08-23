17d180c10116df721a2f5066cd9c91f9bb9f353b
John Watson <johnwatson484@hotmail.com>
2020-08-23 16:44:53 +0100

    Updated player response

--- app/routes/league/player.js
+++ app/routes/league/player.js
@@ -5,6 +5,7 @@ module.exports = [{
   path: '/league/players',
   handler: async (request, h) => {
     const players = await api.get('/league/players', request.state.dl_token)
+    console.log(players)
     return h.view('league/players', { players })
   }
 }]
