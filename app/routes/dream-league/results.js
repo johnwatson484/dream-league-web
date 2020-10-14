75b86673f572cd65f4c28951224c7ad05a57d36e
John Watson <johnwatson484@hotmail.com>
2020-10-14 21:49:52 +0100

    Results api updated

--- app/routes/dream-league/results.js
+++ app/routes/dream-league/results.js
@@ -1,13 +1,10 @@
 const api = require('../../api')
-const joi = require('joi')
-const boom = require('@hapi/boom')
-const ViewModel = require('../../view-models/teamsheet')
 
 module.exports = [{
   method: 'GET',
   path: '/results/edit',
   handler: async (request, h) => {
-    const players = await api.get('/dream-league/players', request.state.dl_token)
+    const players = await api.get('/dream-league/resultsheet', request.state.dl_token)
     return h.view('dream-league/results-edit', { players })
   }
 }]
