6d97f6ad8e12203b9246a06b7fcbaa912fe846b1
John Watson <johnwatson484@hotmail.com>
2020-11-28 17:03:27 +0000

    Results script added

---
+++ app/public/scripts/results.js
@@ -0,0 +1,23 @@
+$(function () {
+  $('#GameWeekId').change()
+})
+
+$('#submit').click(function () {
+  $('#frm-results').submit()
+})
+
+$('.plus').click(function (e) {
+  e.preventDefault()
+  const input = $(this).closest('td').find('input')
+  const value = input.val() === '' ? 0 : parseInt(input.val())
+  input.val(value + 1)
+})
+
+$('.minus').click(function (e) {
+  e.preventDefault()
+  const input = $(this).closest('td').find('input')
+  const value = parseInt(input.val())
+  if (value > 0) {
+    input.val(value - 1)
+  }
+})
