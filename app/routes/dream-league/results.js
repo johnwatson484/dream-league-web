de92758962f04a66f1d22b8487179587b82d9cd8
John Watson <johnwatson484@hotmail.com>
2020-11-29 15:36:17 +0000

    Gameweek dropdown added

--- app/routes/dream-league/results.js
+++ app/routes/dream-league/results.js
@@ -5,7 +5,13 @@ module.exports = [{
   path: '/results/edit',
   handler: async (request, h) => {
     const players = await api.get('/dream-league/results-edit', request.state.dl_token)
-    console.log(players.keepers)
     return h.view('dream-league/results-edit', { players })
+  }
+}, {
+  method: 'POST',
+  path: '/results/edit',
+  handler: async (request, h) => {
+    await api.post('/dream-league/results-edit', request.state.dl_token)
+    return h.redirect('/')
   }
 }]
