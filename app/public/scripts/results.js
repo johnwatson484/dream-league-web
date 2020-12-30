29e213fc4bb2c0f5eb3bdb3bc385461fce0129dc
John Watson <johnwatson484@hotmail.com>
2020-12-30 17:20:52 +0000

    Fix results buttons

--- app/public/scripts/results.js
+++ app/public/scripts/results.js
@@ -8,14 +8,14 @@ $('#submit').click(function () {
 
 $('.plus').click(function (e) {
   e.preventDefault()
-  const input = $(this).closest('td').find('input')
+  const input = $(this).closest('td').find('.result-input')
   const value = input.val() === '' ? 0 : parseInt(input.val())
   input.val(value + 1)
 })
 
 $('.minus').click(function (e) {
   e.preventDefault()
-  const input = $(this).closest('td').find('input')
+  const input = $(this).closest('td').find('.result-input')
   const value = parseInt(input.val())
   if (value > 0) {
     input.val(value - 1)
