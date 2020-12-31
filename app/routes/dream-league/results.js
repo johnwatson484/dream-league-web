a99922294c087f4dd3af37655ad0e01088dd6f0d
John Watson <johnwatson484@hotmail.com>
2020-12-31 13:10:55 +0000

    Validation added

--- app/routes/dream-league/results.js
+++ app/routes/dream-league/results.js
@@ -1,4 +1,6 @@
 const api = require('../../api')
+const joi = require('joi')
+const boom = require('@hapi/boom')
 
 module.exports = [{
   method: 'GET',
@@ -12,9 +14,21 @@ module.exports = [{
 }, {
   method: 'POST',
   path: '/results/edit',
-  handler: async (request, h) => {
-    await api.post('/dream-league/results-edit', request.payload, request.state.dl_token)
-    return h.redirect('/results/edit')
+  options: {
+    validate: {
+      payload: joi.object({
+        gameweekId: joi.number(),
+        conceded: joi.alternatives().try(joi.array().items(joi.object({ teamId: joi.number(), conceded: joi.number() })), joi.string()),
+        goals: joi.alternatives().try(joi.array().items(joi.object({ playerId: joi.number(), goals: joi.number() })), joi.string())
+      }),
+      failAction: async (request, h, error) => {
+        return boom.badRequest(error)
+      }
+    },
+    handler: async (request, h) => {
+      await api.post('/dream-league/results-edit', request.payload, request.state.dl_token)
+      return h.redirect('/results/edit')
+    }
   }
 }]
 
