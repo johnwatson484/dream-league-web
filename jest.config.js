6119d1edb4fff1b14f54aeab1d026d16044352cf
John Watson <johnwatson484@hotmail.com>
2021-02-22 22:11:28 +0000

    Remove coverage

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,3 @@
 module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '**/*.js',
-    '!**/*.test.js'
-  ],
-  coverageDirectory: 'test-output',
-  coverageReporters: [
-    'text-summary',
-    'cobertura',
-    'lcov'
-  ],
-  coveragePathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/test-output/',
-    '<rootDir>/test/',
-    '<rootDir>/jest.config.js',
-    '<rootDir>/jest.setup.js'
-  ],
-  modulePathIgnorePatterns: [
-    'node_modules'
-  ],
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  testEnvironment: 'node'
+  setupFilesAfterEnv: ['./jest.setup.js']
 }
