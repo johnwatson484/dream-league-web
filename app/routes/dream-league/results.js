02928e6062c5391bd53b1ea67435a02a0bb9cf41
John Watson <johnwatson484@hotmail.com>
2021-03-27 17:25:09 +0000

    Badges added

--- app/routes/dream-league/results.js
+++ app/routes/dream-league/results.js
@@ -8,7 +8,7 @@ module.exports = [{
   handler: async (request, h) => {
     const results = await api.get('/dream-league/results', request.state.dl_token)
     const gameweeks = await api.get('/dream-league/gameweeks', request.state.dl_token)
-    console.log(results.scores[0].scorers)
+    console.log(results.scores[0])
     return h.view('dream-league/results', { results, gameweeks })
   }
 }, {
