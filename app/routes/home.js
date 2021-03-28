5e749ad80990bf713c9c75bb06aa96c63e811230
John Watson <johnwatson484@hotmail.com>
2021-03-28 15:54:16 +0100

    Home view updated

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,9 +1,12 @@
+const api = require('../api')
+
 module.exports = [{
   method: 'GET',
   path: '/',
   config: {
   },
-  handler: (request, h) => {
-    return h.view('home')
+  handler: async (request, h) => {
+    const results = await api.get('/dream-league/results', request.state.dl_token)
+    return h.view('home', { results })
   }
 }]
