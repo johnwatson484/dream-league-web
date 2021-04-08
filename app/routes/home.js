b5f7fab63624df348395751b6251f030497b332a
John Watson <johnwatson484@hotmail.com>
2021-04-08 20:33:34 +0100

    Home data updated

--- app/routes/home.js
+++ app/routes/home.js
@@ -7,6 +7,8 @@ module.exports = [{
   },
   handler: async (request, h) => {
     const results = await api.get('/dream-league/results', request.state.dl_token)
-    return h.view('home', { results })
+    const topScorers = await api.get('/dream-league/statistics/top-scorers', request.state.dl_token)
+    const form = await api.get('/dream-league/statistics/form', request.state.dl_token)
+    return h.view('home', { results, topScorers, form })
   }
 }]
