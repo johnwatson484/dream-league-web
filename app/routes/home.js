b4296cc638956b7923f391428acf9dcd00fa8c6a
John Watson <johnwatson484@hotmail.com>
2021-04-10 15:53:41 +0100

    Home page updated

--- app/routes/home.js
+++ app/routes/home.js
@@ -9,6 +9,8 @@ module.exports = [{
     const results = await api.get('/dream-league/results', request.state.dl_token)
     const topScorers = await api.get('/dream-league/statistics/top-scorers', request.state.dl_token)
     const form = await api.get('/dream-league/statistics/form', request.state.dl_token)
+
+    console.log(topScorers)
     return h.view('home', { results, topScorers, form })
   }
 }]
