22abc9e744decfceef146f30799d28da70c21368
John Watson <johnwatson484@hotmail.com>
2021-04-10 15:54:15 +0100

    Remove log

--- app/routes/home.js
+++ app/routes/home.js
@@ -10,7 +10,6 @@ module.exports = [{
     const topScorers = await api.get('/dream-league/statistics/top-scorers', request.state.dl_token)
     const form = await api.get('/dream-league/statistics/form', request.state.dl_token)
 
-    console.log(topScorers)
     return h.view('home', { results, topScorers, form })
   }
 }]
