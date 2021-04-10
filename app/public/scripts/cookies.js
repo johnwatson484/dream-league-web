3fc72424e0a82ff60d1d059928c778a1583ddfd2
John Watson <johnwatson484@hotmail.com>
2021-04-10 20:35:37 +0100

    Dynamic loading of GA added

--- app/public/scripts/cookies.js
+++ app/public/scripts/cookies.js
@@ -8,6 +8,12 @@ acceptButton.addEventListener('click', function (event) {
   questionBanner.setAttribute('hidden', 'hidden')
   event.preventDefault()
   submitPreference(true)
+  const gtmScript = document.createElement('script')
+  gtmScript.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=G-C35GGT2X1B')
+  document.body.appendChild(gtmScript)
+  const gaScript = document.createElement('script')
+  gaScript.setAttribute('src', '/assets/google-analytics.js')
+  document.body.appendChild(gaScript)
 })
 
 function submitPreference (accepted) {
