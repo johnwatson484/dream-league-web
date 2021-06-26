2c5b32a55f3b262e2144afb29b454463e7d1decd
John Watson <johnwatson484@hotmail.com>
2021-06-26 19:38:11 +0100

    Fixture CRUD added

--- app/plugins/router.js
+++ app/plugins/router.js
@@ -12,6 +12,7 @@ const routes = [].concat(
   require('../routes/dream-league/meetings'),
   require('../routes/dream-league/managers'),
   require('../routes/dream-league/cups'),
+  require('../routes/dream-league/fixtures'),
   require('../routes/dream-league/history'),
   require('../routes/public')
 )
---
+++ app/routes/dream-league/fixtures.js
@@ -0,0 +1,97 @@
+const api = require('../../api')
+const joi = require('joi')
+
+module.exports = [{
+  method: 'GET',
+  path: '/dream-league/fixtures',
+  options: {
+    plugins: {
+      crumb: false
+    },
+    handler: async (request, h) => {
+      const fixtures = await api.get('/dream-league/fixtures', request.state.dl_token)
+      return h.view('dream-league/fixtures', { fixtures })
+    }
+  }
+}, {
+  method: 'GET',
+  path: '/dream-league/fixture/create',
+  handler: async (request, h) => {
+    return h.view('dream-league/create-fixture')
+  }
+}, {
+  method: 'POST',
+  path: '/dream-league/fixture/create',
+  options: {
+    validate: {
+      payload: joi.object({
+        cupId: joi.number().integer().required(),
+        gameweekId: joi.number().integer().required(),
+        homeManagerId: joi.number().integer().required(),
+        awayManagerId: joi.number().integer().required(),
+        round: joi.number().integer().required()
+      }),
+      failAction: async (request, h, error) => {
+        return h.view('dream-league/create-fixture', { error, fixture: request.payload }).code(400).takeover()
+      }
+    },
+    handler: async (request, h) => {
+      await api.post('/dream-league/fixture/create', request.payload, request.state.dl_token)
+      return h.redirect('/dream-league/fixtures')
+    }
+  }
+}, {
+  method: 'GET',
+  path: '/dream-league/fixture/edit',
+  handler: async (request, h) => {
+    const fixture = await api.get(`/dream-league/fixture/?fixtureId=${request.query.fixtureId}`, request.state.dl_token)
+    return h.view('dream-league/edit-fixture', { fixture })
+  }
+}, {
+  method: 'POST',
+  path: '/dream-league/fixture/edit',
+  options: {
+    validate: {
+      payload: joi.object({
+        fixtureId: joi.number().integer().required(),
+        cupId: joi.number().integer().required(),
+        gameweekId: joi.number().integer().required(),
+        homeManagerId: joi.number().integer().required(),
+        awayManagerId: joi.number().integer().required(),
+        round: joi.number().integer().required()
+      }),
+      failAction: async (request, h, error) => {
+        return h.view('dream-league/edit-fixture', { fixture: request.payload, error }).code(400).takeover()
+      }
+    },
+    handler: async (request, h) => {
+      await api.post('/dream-league/fixture/edit', request.payload, request.state.dl_token)
+      return h.redirect('/dream-league/fixtures')
+    }
+  }
+}, {
+  method: 'GET',
+  path: '/dream-league/fixture/delete',
+  handler: async (request, h) => {
+    const fixture = await api.get(`/dream-league/fixture/?fixtureId=${request.query.fixtureId}`, request.state.dl_token)
+    return h.view('dream-league/delete-fixture', { fixture })
+  }
+}, {
+  method: 'POST',
+  path: '/dream-league/fixture/delete',
+  options: {
+    validate: {
+      payload: joi.object({
+        fixtureId: joi.number().required()
+      }),
+      failAction: async (request, h, error) => {
+        const fixture = await api.get(`/dream-league/fixture/?fixtureId=${request.query.fixtureId}`, request.state.dl_token)
+        return h.view('dream-league/delete-fixture', { fixture, error }).code(400).takeover()
+      }
+    },
+    handler: async (request, h) => {
+      await api.post('/dream-league/fixture/delete', request.payload, request.state.dl_token)
+      return h.redirect('/dream-league/fixtures')
+    }
+  }
+}]
