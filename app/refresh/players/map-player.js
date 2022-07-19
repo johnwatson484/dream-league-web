6faf795da5881b54ce414756fc2b5aefed17bc72
John Watson <johnwatson484@hotmail.com>
2022-07-19 18:24:29 +0100

    Update trim

--- app/refresh/players/map-player.js
+++ app/refresh/players/map-player.js
@@ -1,9 +1,9 @@
 const mapPlayer = (player) => {
   return {
-    firstName: player['First Name'].trim(),
-    lastName: player.Surname.trim(),
-    position: player.Position.trim(),
-    team: player.Club.trim()
+    firstName: player['First Name'] ? player['First Name'].trim() : undefined,
+    lastName: player.Surname ? player.Surname.trim() : undefined,
+    position: player.Position ? player.Position.trim() : undefined,
+    team: player.Club ? player.Club.trim() : undefined
   }
 }
 
