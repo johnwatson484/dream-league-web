fcf0ae60277991a28065de9e4a58d1ffbe210e0a
John Watson <johnwatson484@hotmail.com>
2023-07-14 22:53:00 +0100

    Fix req

--- app/api/get.js
+++ app/api/get.js
@@ -1,6 +1,6 @@
 const wreck = require('@hapi/wreck')
 const config = require('../config')
-const getConfiguration = require('./get-configuration')
+const { getConfiguration } = require('./get-configuration')
 
 const get = async (url, token) => {
   const { payload } = await wreck.get(`${config.apiHost}${url}`, getConfiguration(token))
