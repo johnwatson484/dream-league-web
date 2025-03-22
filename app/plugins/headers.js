e2df10ca5afd36be1350501ddabea8a93e83ef70
John Watson <john.watson1@defra.gov.uk>
2025-03-22 12:38:53 +0000

    Rename param

--- app/plugins/headers.js
+++ app/plugins/headers.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugin: {
     name: 'headers',
-    register: (server, options) => {
+    register: (server, _options) => {
       server.ext('onPreResponse', (request, h) => {
         const response = request.response
 
