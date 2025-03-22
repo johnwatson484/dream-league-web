ecc6b4035881f42ef87bef88a3777bc1bafedaae
John Watson <john.watson1@defra.gov.uk>
2025-03-22 13:24:28 +0000

    Add headers on boom

--- app/plugins/headers.js
+++ app/plugins/headers.js
@@ -3,19 +3,19 @@ module.exports = {
     name: 'headers',
     register: (server, _options) => {
       server.ext('onPreResponse', (request, h) => {
-        const response = request.response
+        const headers = request.response.isBoom ? request.response.output.headers : request.response?.headers
 
-        if (response.headers) {
-          response.headers['X-Content-Type-Options'] = 'nosniff'
-          response.headers['X-Frame-Options'] = 'DENY'
-          response.headers['X-Robots-Tag'] = 'noindex, nofollow'
-          response.headers['X-XSS-Protection'] = '1; mode=block'
-          response.headers['Cache-Control'] = 'no-cache'
-          response.headers['Cross-Origin-Opener-Policy'] = 'same-origin'
-          response.headers['Cross-Origin-Resource-Policy'] = 'same-site'
-          response.headers['Referrer-Policy'] = 'no-referrer'
-          response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains; preload'
-          response.headers['Permissions-Policy'] = 'camera=(), geolocation=(), magnetometer=(), microphone=(), payment=(), usb=()'
+        if (headers) {
+          headers['X-Content-Type-Options'] = 'nosniff'
+          headers['X-Frame-Options'] = 'DENY'
+          headers['X-Robots-Tag'] = 'noindex, nofollow'
+          headers['X-XSS-Protection'] = '1; mode=block'
+          headers['Cache-Control'] = 'no-cache'
+          headers['Cross-Origin-Opener-Policy'] = 'same-origin'
+          headers['Cross-Origin-Resource-Policy'] = 'same-site'
+          headers['Referrer-Policy'] = 'no-referrer'
+          headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains; preload'
+          headers['Permissions-Policy'] = 'camera=(), geolocation=(), magnetometer=(), microphone=(), payment=(), usb=()'
         }
 
         return h.continue
